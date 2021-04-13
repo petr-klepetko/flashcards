f2e4ee2c41a3c79a7acb3133efab3f7d6a0536c1
Petr Klepetko <klep03@vse.cz>
2021-04-13 20:32:11 +0200

    added file creating

---
+++ Flashcards/file/create/index.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!empty($_GET)) {
+    if (!empty($_GET['filename'])) {
+
+    }
+    else {
+        $response->status = "Filename is not specified"; 
+    }
+}
+else {
+    $response->status = "There are no arguments";
+}
+$myfile = fopen("testfile.txt", "w");
+
+
+$responseJSON = json_encode($response);
+
+echo $responseJSON;
