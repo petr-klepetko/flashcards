8b4870e823946973bd7d565e4ed8acb8eeccad67
Petr Klepetko <klep03@vse.cz>
2021-04-14 12:26:26 +0200

    save

---
+++ Flashcards/selectQ/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Vybrat otázky: </title>
+    <link rel="stylesheet" href="../css/selectQ.css">
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <main>
+        <div class="row center">
+            <p class="text">Vložte soubor s otázkami: </p>
+            <input type="file">
+        </div>
+        <div class="column space-before">
+            <p class="text">Nebo vyberte z již nahraných: </p>
+            <?php 
+                
+            ?>
+        </div>
+    </main>
+    <script id="file" src="../js/selectQ.js"></script>
+</body>
+
+</html>
